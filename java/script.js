161821220e333bce3901a633b32d5b7e7579eba5
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-01-05 15:18:09 -0300

    proximo comit

--- java/script.js
+++ java/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $('#menu').click(function(){
         $(this).toggleClass('fa-times')
+        $('.navbar').toggleClass('nav-toggle')
+    })
+
+    $(window).on('scroll load',function(){
+        $(this).removeClass('fa-times')
+        $('.navbar').removeClass('nav-toggle')
     })
 })
