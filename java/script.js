02b163f5b3c1f8e2c31707f85417c282846d6ac6
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-01-09 20:15:27 -0300

    proximo comit

--- java/script.js
+++ java/script.js
@@ -32,6 +32,13 @@ $(document).ready(function(){
     // smooth scrolling
 
     $('a[href*="#"]').on('click',function(e){
-        
+        e.preventDefault()
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+
+        },
+            500,
+            'linear'
+        )
     })
 })
